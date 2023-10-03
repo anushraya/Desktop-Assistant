9481663a0b0de2bba70dd890e6a7b85a57318be3
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2023-10-03 12:26:07 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,12 +1,53 @@
 import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-print(voices[0].id)
+# print(voices[0].id)
 engine.setProperty('voice',voices[0].id)
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def wishMe():
+    hour=int(datetime.datetime.now().hour)
+    if(hour>=0 and hour<12):
+        speak("Good morning!")  
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+    speak("I am JArvis sir. Plese tell me how may i help you")
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1  #seconds of non speaking audio before a phrase is considered complete
+        audio=r.listen (source) #all are coming from speech recognition module
+
+    try:
+        print("Recognizing...")
+        query= r.recognize_google(audio, Language='en-in')
+        print(f"user said:{query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
 if __name__=="main":
-    speak("harry is a good boy")
+    wishMe()
+    while True:
+        query=takeCommand().lower()
+ #Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('searching wikipedia...')
+            query=query.replace("wikipedia", "")
+            results= wikipedia.summary(query,sentences=3)
+            speak("According to wikipedia")
+            speak(results)
